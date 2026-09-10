fb2c3a08feddb9e0d124e4b249cf892ca524d8f0
S-Yoshi29 <shun.yoshioka.1993@gmail.com>
2026-09-10 20:44:41 +0900

    w/e

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
-};
+const add = (x, y) => x + y;
 
-const subtract = function() {
-	
-};
+const subtract = (first, second) => first - second;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((prev, num) => prev + num, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((prev, num) => prev * num, 1);
 
-};
+const power = (base, exp) => base ** exp;
 
-const power = function() {
-	
-};
+//自分
+// const factorial = (x) => {
+//   const arr = [];
+//   for (let i = 1; i <= x; i++) {
+//     arr.push(i);
+//   }
+//   return multiply(arr);
+// };
 
-const factorial = function() {
-	
-};
+//模範
+// const factorial = (x) => {
+//   let product = 1;
+//   for (let i = 1; i <= x; i++) {
+//     product *= i;
+//   }
+//   return product;
+// };
+
+function factorial(x) {
+  if (x > 1) {
+    return x * factorial(x - 1);
+  } else if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    throw new Error("不適切な入力");
+  }
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const newStr = str.replace(/[ ,.!?]/g, "").toLowerCase();
+  const arr = newStr.split("");
+  const arrReverse = [];
+  for (let i = arr.length; i >= 1; i--) {
+    arrReverse.push(arr[i - 1]);
+  }
+  return (
+    arr.length === arrReverse.length &&
+    arr.every((item, index) => item === arrReverse[index])
+  );
 };
 
 // Do not edit below this line
