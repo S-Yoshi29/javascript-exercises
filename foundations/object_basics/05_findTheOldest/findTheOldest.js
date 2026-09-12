0a8f2413467506b567546c24dbda38b503cadb95
S-Yoshi29 <shun.yoshioka.1993@gmail.com>
2026-09-12 21:42:25 +0900

    owari

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-const findTheOldest = function() {
+// const findTheOldest = function (people) {
+//   return people.reduce(
+//     (prev, person) => {
+//       let age = NaN;
+//       const prevAge = prev.
+//       if (Object.hasOwn(person, "yearOfDeath")) {
+//         age = person.yearOfDeath - person.yearOfBirth;
+//       } else {
+//         age = new Date.getFullYear() - person.yearOfBirth;
+//       }
+//       if (age > prev.) {
+//         return;
+//       } else return prev;
+//     },
+//     0
+// )
 
+const findTheOldest = function (people) {
+  const getAge = (birth, death) => {
+    if (!death) {
+      return new Date().getFullYear() - birth;
+    } else {
+      return death - birth;
+    }
+  };
+  return people.reduce((prev, person) => {
+    if (
+      getAge(person.yearOfBirth, person.yearOfDeath) >
+      getAge(prev.yearOfBirth, prev.yearOfDeath)
+    ) {
+      return person;
+    } else return prev;
+  });
 };
 
 // Do not edit below this line
